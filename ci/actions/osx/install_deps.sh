6ed8f71182727cc2b9c48bf4a4739d06b16a5139
Thiago Silva <thiago@nano.org>
2022-10-14 18:20:48 -0300

    Add OpenSSL support to the MacOS container

--- ci/actions/osx/install_deps.sh
+++ ci/actions/osx/install_deps.sh
@@ -3,5 +3,7 @@
 brew update
 brew install coreutils
 brew cask install xquartz
+brew install openssl@1.1
+brew link openssl@1.1 --force
 sudo util/build_prep/fetch_boost.sh
 util/build_prep/macosx/build_qt.sh
