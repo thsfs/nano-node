2827893efdc68470775e17d6a2bedc8e82fad3e4
Thiago Silva <thiago@nano.org>
2022-10-24 19:10:07 -0300

    Set OPENSSL by the flags

--- ci/actions/osx/install_deps.sh
+++ ci/actions/osx/install_deps.sh
@@ -2,7 +2,7 @@
 
 brew update
 brew install coreutils
-brew cask install xquartz
+brew install xquartz --cask
 brew install openssl@1.1
 brew link openssl@1.1 --force
 sudo util/build_prep/fetch_boost.sh
